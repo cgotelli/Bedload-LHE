0e199f5dab44e9b9a1100dba750a4220d8b8948f
cgotelli <96237432+cgotelli@users.noreply.github.com>
2022-03-16 08:48:51 +0100

    Update paramsComputation.m

--- BedloadCode/src/S2/paramsComputation.m
+++ BedloadCode/src/S2/paramsComputation.m
@@ -8,12 +8,12 @@ if strcmp(camera, "Halle")
     fps         = 45;           % Frame rate for acquisition in the flume
     distMaxVel  = 150;          % Max distance traveled by a particle between images. To avoid impossible pairs.
     distMinVel  = 70;           % Min distance traveled by a particle between images. To avoid impossible pairs.
-    distMinIsol = 40;           % Separation to define isolated particles. Necessary for getting a mean velocity.
+    distMinIsol = 60;           % Separation to define isolated particles. Necessary for getting a mean velocity.
     areamin     = 0;            % Lower threshold area to consider a particle in the counting process. In terms of mean.
-    areamax     = 100;            % Upper threshold area to consider a particle in the counting process. In terms of mean.
-    difs_th     = 0.1;         % logarithmic size threshold for identifying two similar particles.
+    areamax     = 100;          % Upper threshold area to consider a particle in the counting process. In terms of mean.
+    difs_th     = 0.1;          % logarithmic size threshold for identifying two similar particles.
     x_dev       = 20;           % maximum allowed value of horizontal deviation between two consecutive images (in px).
-    lim_width   = 0.05;          % fraction of the image to exclude in the x axis for each border.
+    lim_width   = 0.05;         % fraction of the image to exclude in the x axis for each border.
     lim_height  = 0.05;         % fraction of the image to exclude in the y axis for each border. 
     imwidth     = 2712;         % image width
     imheight    = 500;          % image height
