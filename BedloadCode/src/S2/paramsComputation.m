2848cffca95f9f7772ad3ce3373843573a461d0a
Clemente Gotelli <96237432+cgotelli@users.noreply.github.com>
2022-08-02 13:15:35 +0200

    Update paramsComputation.m

--- BedloadCode/src/S2/paramsComputation.m
+++ BedloadCode/src/S2/paramsComputation.m
@@ -5,7 +5,7 @@ if strcmp(camera, "Halle")
     
     maxparticles= 1000;         % Maximum number of particles to process by frame (~15)
     
-    fps         = 45;           % Frame rate for acquisition in the flume
+    fps         = 50;           % Frame rate for acquisition in the flume
     distMaxVel  = 150;          % Max distance traveled by a particle between images. To avoid impossible pairs.
     distMinVel  = 70;           % Min distance traveled by a particle between images. To avoid impossible pairs.
     distMinIsol = 30;           % Separation to define isolated particles. Necessary for getting a mean velocity.
